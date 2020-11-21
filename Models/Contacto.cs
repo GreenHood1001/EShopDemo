518f08994fb27cced0725e404f0e2fb634378643
Omar Poma <omarpv007@gmail.com>
2020-11-20 21:44:22 -0500

    Corrección Contacto

--- Models/Contacto.cs
+++ Models/Contacto.cs
@@ -1,10 +1,9 @@
 using System;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
-
 namespace EShopDemo.Models
 {
-     [Table("t_contacto")]
+    [Table("t_contacto")]
     public class Contacto
     {
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
